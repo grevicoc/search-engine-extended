29183a51e121fbebacc527c1907bbf0053fa8b8b
Muhammad Tito P <cocabc1@gmail.com>
2021-07-17 23:30:50 +0700

    tf added

--- src/main.go
+++ src/main.go
@@ -9,4 +9,9 @@ func main(){
 	var testArray = []float32{0.27,11.1,5.4,6.02,3.01,3.01}
 	functions.HeapSort(testArray)
 	fmt.Println(testArray)
+
+	testDoc := "Lorem ipsum dolor amet ahay de kocak ipsum lorem test aja ini mah ahay ahay de"
+		
+	value,_ := functions.Tf(testDoc)
+	fmt.Println(value)
 }
