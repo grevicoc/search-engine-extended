34d31491749773d67926562a48c4629eb41bfd6e
Muhammad Tito P <cocabc1@gmail.com>
2021-07-20 00:28:23 +0700

    adding idf fix, cosine sim

---
+++ src/functions/tf-idf.go
@@ -0,0 +1,132 @@
+package functions
+
+import (
+	"strings"
+	"math"
+)
+
+//fungsi antara untuk nentuin apakah suatu char ada di suatu kumpulan char
+func contains(char rune, word []rune) (bool){
+	for _,c := range word{
+		if char==c {
+			return true
+		}
+	}
+	return false
+}
+
+//fungsi untuk menghitung suatu dokumen ada kata apa saja dan jumlahnya berapa
+func countWords(doc string) (map[string]int, error){
+	//inisialisasi retval
+	var retVal map[string]int = map[string]int{}
+
+	//inisialisasi huruf yang bisa menjadi "pengganggu" di sebuah kata
+	var tandaBaca = []rune{'.','>',',','<','/','?','\'','"',';',':','\\','|',']','}','[','{','=','+','-','_',')','(','*','&','&','^','%','$','#','@','!','`','~',' ','\n'}
+
+	//menyiapkan tempWord untuk variabel sementara sebuah kata dan iterasi tiap char di document
+	var tempWord string = ""
+	for _, char := range doc{
+
+		//sebelum mengupdate, pastikan kata-katanya udh di-trim dengan dilowercase dan dihilangkan tanda bacanya
+		if !contains(char,tandaBaca) {
+			tempWord = tempWord + string(char)	
+		}
+
+		if contains(char,tandaBaca)  {
+			tempWord = strings.ToLower(tempWord)
+			retVal[tempWord]++
+			tempWord = ""
+		}
+	}
+
+	return retVal, nil
+}
+
+func countLogOfFreq(bagOfWord map[string]int) (map[string]float32, error){
+	//hitung total kata
+	var totalWords int = 0
+	for _, val:= range bagOfWord{
+		totalWords+= val
+	}
+	
+	//inisialisasi retval
+	var retVal map[string]float32 = map[string]float32{}
+
+	for key, val:= range bagOfWord{
+		// retVal[key] = float32(val) / float32(totalWords)
+		retVal[key] = float32(math.Log(float64(1 + val)))
+	}
+
+	return retVal, nil
+}
+
+func Tf(doc string) (map[string]float32, error){
+	bow,_ := countWords(doc)
+	result,_ := countLogOfFreq(bow)
+
+	return result, nil
+}
+
+//menerima inputan slice of string yang berisi banyak dokumen dan inputan kata-kata query. Akan dicari idf tiap-tiap kata
+//ASUMSI arrOfQuery telah "bersih" alias tidak ada stopwords
+func Idf(documents []string, arrOfQuery map[string]int) (map[string]float32)  {
+
+	//pertama-tama bikin slice of BoW dari masing-masing document dari documents
+	var bagOfWords []map[string]int
+	for _, doc := range documents{
+		bagOfWord,_ := countWords(doc)
+		bagOfWords = append(bagOfWords, bagOfWord)
+	}
+
+	//selanjutnya bikin retval
+	var retVal map[string]float32 = map[string]float32{}
+	for key := range arrOfQuery {		var count = 0
+		for _, bow := range bagOfWords{
+			//ngecek apakah di dalam bow tersebut ada word yg dimaksud
+			if bow[key]!=0{
+				count++
+			}
+		}
+		retVal[key] = float32(math.Log(float64(len(documents)) / float64(count)))
+		
+	}
+
+	return retVal
+}
+
+func TfIdf(valueTF map[string]float32, valueIDF map[string]float32) (map[string]float32) {
+	var retVal map[string]float32 = map[string]float32{}
+
+	for key,val := range valueIDF{
+		if valueTF[key]!=0.0{
+			retVal[key] = valueTF[key] * val
+		}else{
+			retVal[key] = 0.0
+		}
+	}
+
+	return retVal
+}
+
+//harusnya di sini vektor query sama vectorDocument isinya dah sama
+func CosineSim(query map[string]int, vectorDocument map[string]float32) (float32){
+	var dotProduct float32 = 0
+	for key,val := range query{
+		dotProduct += vectorDocument[key] * float32(val)
+	}
+
+	var doubleLineQuery float32 = 0
+	for _,val := range query{
+		doubleLineQuery += float32(val*val)
+	}
+	doubleLineQuery = float32(math.Sqrt(float64(doubleLineQuery)))
+
+	var doubleLineDocument float32 = 0
+	for _,val := range vectorDocument{
+		doubleLineDocument += val*val
+	}
+	doubleLineDocument = float32(math.Sqrt(float64(doubleLineDocument)))
+
+
+	return dotProduct / (doubleLineQuery*doubleLineDocument)
+}
--- src/main.go
+++ src/main.go
@@ -4,19 +4,14 @@ import (
 	"fmt"
 	"search-engine-extended/src/functions"
 )
-
+//TODO cosine sim
 func main(){
 	var testArray = []float32{0.27,11.1,5.4,6.02,3.01,3.01}
 	functions.HeapSort(testArray)
 	fmt.Println(testArray)
 
-	testDoc := "Lorem ipsum dolor amet ahay de kocak ipsum lorem test aja ini mah ahay ahay de"
-		
-	value,_ := functions.Tf(testDoc)
-	fmt.Println(value)
-
 	testDoc1 := `
-	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 	`
 	testDoc2 :=`
 	It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
@@ -31,8 +26,16 @@ func main(){
 	testDoc5 := `
 	Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 	`
+	valueTF,_ := functions.Tf(testDoc1)
+	fmt.Println(valueTF)
+
 	docs := []string{testDoc1,testDoc2,testDoc3,testDoc4,testDoc5}
-	query := []string{"lorem","ipsum","sit"}
+	query := map[string]int{"lorem":1,"ipsum":1,"sit":1, "dummy":1}
 
 	fmt.Println(functions.Idf(docs,query))
+	var valueIDF = functions.Idf(docs,query)
+	
+	var vectorDocument = functions.TfIdf(valueTF,valueIDF)
+	fmt.Println(vectorDocument)
+	fmt.Println(functions.CosineSim(query,vectorDocument))
 }
